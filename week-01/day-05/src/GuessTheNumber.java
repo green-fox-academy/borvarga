66651d27fb338f49e233a1b79d10e02d60686184
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-05 14:30:39 +0200

    completed GuessTheNumber task

--- week-01/day-05/src/GuessTheNumber.java
+++ week-01/day-05/src/GuessTheNumber.java
@@ -4,12 +4,20 @@ public class GuessTheNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int theNumber = 69;
-        System.out.println("Guess my number!");
-        int guess = scanner.nextInt();
-        if (guess < theNumber) {
-            System.out.println("The stored number is higher. ");
-        } else if (guess > theNumber) {
-            System.out.println("The stored number is lower.");
+        int guess ;
+        boolean win = false;
+        while (win == false) {
+            System.out.println("Guess my number! ");
+            guess = scanner.nextInt();
+            if (guess == theNumber) {
+                win = true;
+            } else if (guess < theNumber) {
+                System.out.println("The stored number is higher.");
+            } else if (guess > theNumber) {
+                System.out.println("The stored number is lower.");
+            }
         }
+        System.out.println("You guessed the number: " + theNumber);
+
     }
 }
