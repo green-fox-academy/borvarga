ec03172aebaa022f255dcf021921936b52054834
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-08 14:27:49 +0200

    Solved Greet

---
+++ week-02/day-01/src/Doubling.java
@@ -0,0 +1,16 @@
+public class Doubling {
+    public static void main(String[] args) {
+        doubling();
+        System.out.println(doubling());
+
+
+
+    }
+
+
+    public static int doubling(){
+        System.out.println("asd");
+        int baseNum = 123;
+        return baseNum * 2;
+    }
+}
---
+++ week-02/day-01/src/Greet.java
@@ -0,0 +1,13 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "Greenfox";
+        greet();
+        System.out.print(al);
+
+
+
+    }
+    public static void greet(){
+        System.out.print("Greetings dear, ");
+    }
+}
