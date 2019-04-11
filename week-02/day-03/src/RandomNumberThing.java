f844014e8eec64c2889383c2942b66928fb740de
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-11 11:48:15 +0200

    solved numberadder

---
+++ week-02/day-03/src/RandomNumberThing.java
@@ -0,0 +1,2 @@
+public class RandomNumberThing {
+}
---
+++ week-02/day-04/src/SumDigit.java
@@ -0,0 +1,13 @@
+public class SumDigit {
+    public static void main(String[] args) {
+        int sum = 69;
+
+        for (int i = 1; i <= 100; i++) {
+
+            sum += i;
+
+        }
+
+        System.out.println("The sum is " + sum);
+    }
+}
