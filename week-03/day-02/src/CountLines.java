1dc1a5c84a6bd5bf47a451f32a9c7ec5f3d5ef66
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-16 12:21:30 +0200

    added an empty testfile

---
+++ week-03/day-02/src/CountLines.java
@@ -0,0 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CountLines {
+    public static void main(String[] args) {
+        try {
+            Path newPath = Paths.get("assets/testfile.txt");
+            List<String> list = Files.readAllLines(newPath);
+            System.out.println(list.size());
+        } catch (Exception e) {
+            System.out.println(0);
+        }
+    }
+}
