6ef08a89acbe60c49df945d16f83d6ff36daa36e
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-17 11:48:47 +0200

    solved PostIt

---
+++ week-03/day-03/src/PostIt.java
@@ -0,0 +1,11 @@
+public class PostIt {
+    public PostIt(String backgroundColor, String text, String textcolor){
+
+    }
+
+    public static void main(String[] args) {
+        PostIt exampleOne = new PostIt("Orange", "Idea 1","blue");
+        PostIt exampleTwo = new PostIt("Pink", "Awesome","black");
+        PostIt exampleThree = new PostIt("Yellow", "Superb!","green");
+    }
+}
