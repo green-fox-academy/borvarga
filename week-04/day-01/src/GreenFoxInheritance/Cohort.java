8b04cd35802e6ef7863e7437dc49e17827e358bf
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-23 14:51:45 +0200

    added main.java

--- week-04/day-01/src/GreenFoxInheritance/Cohort.java
+++ week-04/day-01/src/GreenFoxInheritance/Cohort.java
@@ -20,49 +20,8 @@ public class Cohort {
     void addMentor(Mentor mentor){
         mentors.add(mentor);
     }
+    
     void info(){
         System.out.println("The " + nameCohort + "cohort has " + students.size() + "students and " + mentors.size() + "." );
-    }
-
-    public void main(String[] args) {
-
-        Person mark = new Person("Mark", 46, "male");
-        people.add(mark);
-        Person jane = new Person();
-        people.add(jane);
-        Student john = new Student("John Doe", 20, "male", "BME");
-        people.add(john);
-        Student student = new Student();
-        people.add(student);
-        Mentor gandhi = new Mentor("Gandhi", 148, "male", "senior");
-        people.add(gandhi);
-        Mentor mentor = new Mentor();
-        people.add(mentor);
-        Sponsor sponsor = new Sponsor();
-        people.add(sponsor);
-        Sponsor elon = new Sponsor("Elon Musk", 46, "male", "SpaceX");
-        people.add(elon);
-
-        student.skipDays(3);
-
-        for (int i = 0; i < 5; i++) {
-            elon.hire();
-        }
-
-        for (int i = 0; i < 3; i++) {
-            sponsor.hire();
-        }
-
-        for(Person person : people) {
-            person.introduction();
-            person.getGoal();
-        }
-
-        Cohort awesome = new Cohort("AWESOME");
-        awesome.addStudent(student);
-        awesome.addStudent(john);
-        awesome.addMentor(mentor);
-        awesome.addMentor(gandhi);
-        awesome.info();
     }
 }
