9b56f7a086c506343ef48a84e0b8c7742991ae3b
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-23 14:29:22 +0200

    gave all methods to Cohort.java

--- week-04/day-01/src/GreenFoxInheritance/Cohort.java
+++ week-04/day-01/src/GreenFoxInheritance/Cohort.java
@@ -1,4 +1,25 @@
 package GreenFoxInheritance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cohort {
+    String nameCohort;
+    List<Student> students = new ArrayList<>();
+    List<Mentor> mentors = new ArrayList<>();
+
+    Cohort (String nameCohort){
+        this.nameCohort = nameCohort;
+    }
+
+    void addStudent(Student student){
+        students.add(student);
+    }
+
+    void addMentor(Mentor mentor){
+        mentors.add(mentor);
+    }
+    void info(){
+        System.out.println("The " + nameCohort + "cohort has " + students.size() + "students and " + mentors.size() + "." );
+    }
 }
