ead051bed5c0ad0b586471c49f7e5ae62a575a6f
Zsombor Varga <vargazsombor0331@gmail.com>
2019-04-23 14:07:33 +0200

    added methods to Sponsor.java

--- week-04/day-01/src/GreenFoxInheritance/Sponsor.java
+++ week-04/day-01/src/GreenFoxInheritance/Sponsor.java
@@ -1,4 +1,34 @@
 package GreenFoxInheritance;
 
-public class Sponsor {
+public class Sponsor extends Person{
+    String company;
+    int hiredStudents;
+
+    Sponsor(){
+    }
+
+    Sponsor(String name, String gender, int age, String company){
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.company = company;
+    }
+
+
+    @Override
+    void introduction() {
+        super.introduction();
+        System.out.println("Hi, I'm " + name + age + " year old " + gender + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+
+    }
+
+    int hire(int a){
+        return hiredStudents + a;
+    }
+
+    @Override
+    void getGoal() {
+        super.getGoal();
+        System.out.println("Hire brilliant junior software developers.");
+    }
 }
